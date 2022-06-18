52ff3b295bb2bd7b1ef200b1a9537e4f53f571d1
Jason Mason <jasonjaymason@gmail.com>
2022-06-17 20:22:43 -0600

    refactor player class to be more readable

--- classes/player.js
+++ classes/player.js
@@ -18,70 +18,28 @@ export class Player {
     this.lasers = {}
   }
 
-  render(ctx) {
-    ctx.shadowBlur = 17
-    ctx.translate(this.position.x, this.position.y)
-    ctx.shadowColor = 'rgba(255, 255, 255, 0.46)'
-    ctx.rotate(this.rotation)
-    ctx.drawImage(
-      this.imgEl,
-      -this.width / 2,
-      -this.height / 2,
-      this.width,
-      this.height
-    )
-    ctx.resetTransform()
+  activatePrimaryThrusters() {
+    let factor = Math.pow(0.5, Math.abs(this.velocity.y + this.velocity.x))
+    this.velocity.x =
+      this.velocity.x +
+      factor * (Math.cos(this.rotation - Math.PI / 2) * this.thrust)
+    this.velocity.y =
+      this.velocity.y +
+      factor * (Math.sin(this.rotation - Math.PI / 2) * this.thrust)
   }
-
-  update(ctx, frame) {
-    if (this.controles.thrust) {
-      let factor = Math.pow(0.5, Math.abs(this.velocity.y + this.velocity.x))
-      this.velocity.x =
-        this.velocity.x +
-        factor * (Math.cos(this.rotation - Math.PI / 2) * this.thrust)
-      this.velocity.y =
-        this.velocity.y +
-        factor * (Math.sin(this.rotation - Math.PI / 2) * this.thrust)
-    }
-    if (
-      this.position.y <= this.boundaryPadding &&
-      Math.sign(this.velocity.y) === -1
-    ) {
-      this.velocity.y = (this.velocity.y / 2) * -1
-    }
-    if (
-      this.position.y >= innerHeight - this.boundaryPadding &&
-      Math.sign(this.velocity.y) === 1
-    ) {
-      this.velocity.y = (this.velocity.y / 2) * -1
-    }
-    if (
-      this.position.x <= this.boundaryPadding &&
-      Math.sign(this.velocity.x) === -1
-    ) {
-      this.velocity.x = (this.velocity.x / 2) * -1
-    }
-    if (
-      this.position.x >= innerWidth - this.boundaryPadding &&
-      Math.sign(this.velocity.x) === 1
-    ) {
-      this.velocity.x = (this.velocity.x / 2) * -1
-    }
+  activateRotationThrusters() {
     if (this.controles.right && !this.controles.left) {
       this.rotation = this.rotation + 0.1
     } else if (this.controles.left && !this.controles.right) {
       this.rotation = this.rotation - 0.1
     }
-    this.position.x += this.velocity.x
-    this.position.y += this.velocity.y
-
-    //update lasers
-    //get the keys of the lasers object
+  }
+  fireLaser(frame) {
     let laserKeys = Object.keys(this.lasers)
     //get the frame that the last laser was fired
     let lastLaserFrame = parseInt(laserKeys[laserKeys.length - 1]) || -10
     //see if the fire button is pushed and if the gun is ready to shoot again
-    if (this.controles.fire && lastLaserFrame + 10 < frame) {
+    if (lastLaserFrame + 10 < frame) {
       const rotation = this.rotation
       //set the position that the laser appears relative to this player
       const position = {
@@ -96,6 +54,11 @@ export class Player {
       })
       this.lasers[frame] = laser
     }
+  }
+  updateLaserPositions(ctx) {
+    //update lasers
+    //get the keys of the lasers object
+    let laserKeys = Object.keys(this.lasers)
 
     //if there are current lasers, then update their position and delete them if they are off screen
     laserKeys &&
@@ -121,6 +84,72 @@ export class Player {
           this.lasers[laser].update(ctx)
         }
       })
+  }
+
+  handleCanvasBoundaries() {
+    if (
+      this.position.y <= this.boundaryPadding &&
+      Math.sign(this.velocity.y) === -1
+    ) {
+      this.velocity.y = (this.velocity.y / 2) * -1
+    }
+    if (
+      this.position.y >= innerHeight - this.boundaryPadding &&
+      Math.sign(this.velocity.y) === 1
+    ) {
+      this.velocity.y = (this.velocity.y / 2) * -1
+    }
+    if (
+      this.position.x <= this.boundaryPadding &&
+      Math.sign(this.velocity.x) === -1
+    ) {
+      this.velocity.x = (this.velocity.x / 2) * -1
+    }
+    if (
+      this.position.x >= innerWidth - this.boundaryPadding &&
+      Math.sign(this.velocity.x) === 1
+    ) {
+      this.velocity.x = (this.velocity.x / 2) * -1
+    }
+  }
+
+  updateShipPosition() {
+    this.handleCanvasBoundaries()
+    this.position.x += this.velocity.x
+    this.position.y += this.velocity.y
+  }
+
+  controleShip(frame) {
+    if (this.controles.left || this.controles.right) {
+      this.activateRotationThrusters()
+    }
+    if (this.controles.thrust) {
+      this.activatePrimaryThrusters()
+    }
+    if (this.controles.fire) {
+      this.fireLaser(frame)
+    }
+  }
+
+  render(ctx) {
+    ctx.shadowBlur = 17
+    ctx.translate(this.position.x, this.position.y)
+    ctx.shadowColor = 'rgba(255, 255, 255, 0.46)'
+    ctx.rotate(this.rotation)
+    ctx.drawImage(
+      this.imgEl,
+      -this.width / 2,
+      -this.height / 2,
+      this.width,
+      this.height
+    )
+    ctx.resetTransform()
+  }
+
+  update(ctx, frame) {
+    this.controleShip(frame)
+    this.updateShipPosition()
+    this.updateLaserPositions(ctx)
 
     this.render(ctx)
   }
